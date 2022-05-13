5ecdf5fedff26c455ba8791effa829161ef3f0d5
Babykala <babykala3@gmail.com>
2022-05-13 11:16:26 +0530

    third commit

---
+++ router/mentor_student.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router=express.Router();
+
+const getModule=require('../module/mentor_student.js');
+
+router.post('/create',getModule.createMentor);
+
+router.post('/post',getModule.createStudent);
+
+//router.put('/update/:mentorId',getModule.updateMentor);
+
+router.put('/update/:studentId',getModule.updateStudent);
+
+router.delete('/remove/:studentId',getModule.deleteStudent);
+
+//router.get('/get',getModule.getStudent);
+
+router.get('/get',getModule.getmentees);
+
+
+module.exports=router;
