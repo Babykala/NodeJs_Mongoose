761bce2ba7c7c89d869fbaa76da64f970af74612
Babykala <babykala3@gmail.com>
2022-05-15 07:14:16 +0530

    Update

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const mongo=require('./shared');
-const getRouter=require('./router/hall');
+
 const mentor_student_Router=require('./router/mentor_student');
 dotenv.config();
 const app=express();
 
 app.use(express.json()); //inbuilt middleware //convert req body into json format
-mongo.connect();
+
 mongo.connectMongoose();
 
-app.use('/users',getRouter);
+
 app.use('/mentor',mentor_student_Router);
 app.use('/student',mentor_student_Router);
 
-app.listen('3000');
+const port=process.env.PORT || 3000;
+
+app.listen(port,function(){
+    console.log("Server started successfully");
+});
 
--- shared.js
+++ shared.js
@@ -1,16 +1,6 @@
-const {MongoClient}=require('mongodb');
 const mongoose=require('mongoose'); 
 
 module.exports = {
-    selectedDb:{},
-    async connect(){
-        try{
-            const client=await MongoClient.connect(process.env.MONGO_DB_DRIVER);
-            this.selectedDb=client.db('vbrs');
-            console.log(this.selectedDb)
-        }
-        catch(error){console.log(error)}
-    },
     async connectMongoose(){
         try{
             mongoose.connect(process.env.MONGOOSE_CONNECTION_URL);
